e36e8290a3136cfce59ac465dc743a0d360dd112
duyuqing <1403726672@qq.com>
2017-08-12 00:39:17 +0800

    js、css剥离

---
+++ src/views/demo/navigator/index.js
@@ -0,0 +1,29 @@
+const index = {
+  name: 'demo-base',
+
+  data () {
+    return {
+      spanLeft: 5,
+      spanRight: 19
+    }
+  },
+  computed: {
+    iconSize () {
+      return this.spanLeft === 5 ? 14 : 24;
+    }
+  },
+  methods: {
+    toggleClick () {
+      if (this.spanLeft === 5) {
+        this.spanLeft = 2;
+        this.spanRight = 22;
+      } else {
+        this.spanLeft = 5;
+        this.spanRight = 19;
+      }
+    }
+  }
+
+};
+
+export default index;
